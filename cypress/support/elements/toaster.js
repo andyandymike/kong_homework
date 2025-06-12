27c33b58b44a897719606f0927a26a5dd533ff58
andyandymike <564458508@qq.com>
2025-06-12 20:35:13 +0800

    extract selectors

--- cypress/support/elements/toaster.js
+++ cypress/support/elements/toaster.js
@@ -1,9 +1,14 @@
+import { toaster } from '../selectors/selector';
+
 class Toaster {
     //close toaster if it exists
     closeToaster() {
-        cy.get('.k-toaster', { timeout: 0 }).then($el => {
+        //wait for toaster to appear
+        cy.wait(1000);
+        //close toaster if it exists
+        cy.get(toaster.toaster_root_class, { timeout: 0, multiple: true }).each(($el, index, $list) => {
             if ($el.length && $el.is(':visible')) {
-                cy.wrap($el).find('[data-testid="kui-icon-svg-close-icon"]').click();
+                cy.wrap($el).find(toaster.toaster_close_icon).click();
             } else {
                 cy.log('Popup is already gone or hidden');
             }
